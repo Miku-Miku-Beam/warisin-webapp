cbbff066f5f977bf5bf09a2c43268e78bdc1bb65
hnzbyte <hambasahaya0303@gmail.com>
2025-07-25 19:02:22 +0700

    refactor: reorganize imports and remove unused ProgressBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 
-import HeroSection from './components/HeroSection';
-import HowItWorksSection from './components/HowItWorksSection';
-import TestimonialSection from './components/TestimonialSection';
-import LatestInternshipSection from './components/LatestInternshipSection';
-import CallToActionSection from './components/CallToActionSection';
 import Navbar from '@/lib/components/Navbar';
 import ProgressBar from '@/lib/components/ProgressBar';
+import CallToActionSection from './components/CallToActionSection';
+import HeroSection from './components/HeroSection';
+import HowItWorksSection from './components/HowItWorksSection';
+import LatestInternshipSection from './components/LatestInternshipSection';
+import TestimonialSection from './components/TestimonialSection';
 
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-white">
-      <ProgressBar />
       <Navbar />
       <HeroSection />
       <HowItWorksSection />
